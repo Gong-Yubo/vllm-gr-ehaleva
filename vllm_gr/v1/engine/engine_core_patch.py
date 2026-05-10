b5bf0f6bf79e51bae93f54ffac6f64d908924e8a
haim moushkatel <haim.moushkatel@huawei.com>
2026-05-10 10:07:48 +0300

    Some minor changes

--- vllm_gr/v1/engine/engine_core_patch.py
+++ vllm_gr/v1/engine/engine_core_patch.py
@@ -55,8 +55,12 @@ def _compute_beam_prefix_groups(req_ids: list[str], requests: dict, block_size: 
 
     # Group requests by beam group id (priority)
     beam_groups: dict[int, list[str]] = {}
+    cnt = 0
     for req_id in req_ids:
         priority = requests[req_id].priority
+        if priority == 0:
+            priority = cnt
+            cnt += 1
         if priority not in beam_groups:
             beam_groups[priority] = []
         beam_groups[priority].append(req_id)
@@ -131,10 +135,18 @@ def _apply_pbsc_routing(enable_pbsc: bool, output, beam_groups: list, requests: 
         t_block_aligned = (orig_t_prefix // block_size) * block_size
         # 2. Sub-group by exact cache_hit for PBSC tensor shaping
         cache_hit_groups = {}
+        cnt = 0
         for req_id in group_req_ids:
             req = requests[req_id]
-            # Use actual computed tokens to correctly handle chunked prefill
-            cache_hit = getattr(req, "num_computed_tokens", 0)
+            # vLLM updates req.num_computed_tokens during schedule(). 
+            # By subtracting the schedule, we get the exact number of tokens 
+            # that were in the KV cache BEFORE this step, preventing OOB errors.
+            cache_hit = req.num_computed_tokens - output.num_scheduled_tokens[req_id]
+            # cache_hit = cache_hit // block_size * block_size
+            if(cnt % 128 == 0):
+                print(f"token num = {req.num_tokens} req.num_computed_tokens {req.num_computed_tokens}, output.num_scheduled_tokens[req_id]={output.num_scheduled_tokens[req_id]}")                
+                print(f"CACH HIT {cache_hit}, orig_t_prefix={orig_t_prefix}, req_id={req_id}")
+            cnt += 1
             # If the request already has the entire shared prefix in cache,
             # it doesn't need PBSC tail sharing.
             if cache_hit >= orig_t_prefix:
@@ -158,7 +170,8 @@ def _apply_pbsc_routing(enable_pbsc: bool, output, beam_groups: list, requests: 
             min_scheduled = output.num_scheduled_tokens[leader_id]
             for child_id in valid_children:
                 min_scheduled = min(min_scheduled, output.num_scheduled_tokens[child_id])
-            t_prefix = min(orig_t_prefix, t_cache_hit + min_scheduled)
+            # Ensure each child computes at least 1 token to prevent num_scheduled_tokens == 0
+            t_prefix = min(orig_t_prefix, t_cache_hit + min_scheduled - 1)
 
             # If the cache hit missed full blocks we thought were shared,
             # restrict the sharing to what is actually cached.
